dbc96142868c6a0b2455f895771f90e9df0a43bd
anthoox <kray.a4@gmail.com>
2025-10-21 17:13:23 +0200

    feat: maquetando vista views/services/services.php

--- app/controllers/servicesController.php
+++ app/controllers/servicesController.php
@@ -11,8 +11,6 @@ class servicesController{
   public function alls()
   {
     // Lógica para mostrar el formulario de creación de usuario
-    echo 'Estamos en apps.php';
-
     require_once __DIR__ . '/../views/services/services.php';
   }
 }
--- app/views/layout.php
+++ app/views/layout.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Gestor de Claves</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="http://localhost/keys/public/css/style.css">
 </head>
 
 <body>
@@ -33,7 +33,7 @@
     ?>
 
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
