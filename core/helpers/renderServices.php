c2925b80c8cf2c7fb32039e1fa26b6bd820d6c7c
anthoox <kray.a4@gmail.com>
2025-11-05 11:29:17 +0100

    corrección de tabulaciones

--- core/helpers/renderServices.php
+++ core/helpers/renderServices.php
@@ -26,14 +26,13 @@ function renderServices(array $services)
           <h5 class='card-title mb-0'>{$service_name}</h5>
           <div>
             <div class='btn btn-sm btn-show p-1 edit-service-btn' data-bs-toggle='modal' 
-  data-bs-target='#editServiceModal'
-  data-id='{$service_id}'
-  data-name='{$service_name}'
-  data-user='{$user_name}'
-
-  data-password='********'>
+            data-bs-target='#editServiceModal'
+            data-id='{$service_id}'
+            data-name='{$service_name}'
+            data-user='{$user_name}'
+            data-password='********'>
               <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 1792 1408'>
-              <path fill='#fbfbfb' d='m888 1056l116-116l-152-152l-116 116v56h96v96zm440-720q-16-16-33 1L945 687q-17 17-1 33t33-1l350-350q17-17 1-33m80 594v190q0 119-84.5 203.5T1120 1408H288q-119 0-203.5-84.5T0 1120V288Q0 169 84.5 84.5T288 0h832q63 0 117 25q15 7 18 23q3 17-9 29l-49 49q-14 14-32 8q-23-6-45-6H288q-66 0-113 47t-47 113v832q0 66 47 113t113 47h832q66 0 113-47t47-113V994q0-13 9-22l64-64q15-15 35-7t20 29m-96-738l288 288l-672 672H640V864zm444 132l-92 92l-288-288l92-92q28-28 68-28t68 28l152 152q28 28 28 68t-28 68' />
+                <path fill='#fbfbfb' d='m888 1056l116-116l-152-152l-116 116v56h96v96zm440-720q-16-16-33 1L945 687q-17 17-1 33t33-1l350-350q17-17 1-33m80 594v190q0 119-84.5 203.5T1120 1408H288q-119 0-203.5-84.5T0 1120V288Q0 169 84.5 84.5T288 0h832q63 0 117 25q15 7 18 23q3 17-9 29l-49 49q-14 14-32 8q-23-6-45-6H288q-66 0-113 47t-47 113v832q0 66 47 113t113 47h832q66 0 113-47t47-113V994q0-13 9-22l64-64q15-15 35-7t20 29m-96-738l288 288l-672 672H640V864zm444 132l-92 92l-288-288l92-92q28-28 68-28t68 28l152 152q28 28 28 68t-28 68' />
               </svg>
             </div>
             <div class='btn btn-sm btn-show p-1 del-service-btn' data-bs-toggle='modal'  data-bs-target='#delServiceModal' data-id='{$service_id}'>
@@ -51,12 +50,14 @@ function renderServices(array $services)
               <p class='mb-2'><strong>Usuario:</strong> {$user_name}</p>
             </div>
             <div>
-                     <button class='btn btn-sm btn-copy p-1'><svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24'>
-                    <g fill='none' stroke='#666666' stroke-linecap='round' stroke-linejoin='round' stroke-width='2'>
-                      <path d='M7 9.667A2.667 2.667 0 0 1 9.667 7h8.666A2.667 2.667 0 0 1 21 9.667v8.666A2.667 2.667 0 0 1 18.333 21H9.667A2.667 2.667 0 0 1 7 18.333z' />
-                      <path d='M4.012 16.737A2 2 0 0 1 3 15V5c0-1.1.9-2 2-2h10c.75 0 1.158.385 1.5 1' />
-                    </g>
-                  </svg></button>
+              <button class='btn btn-sm btn-copy p-1'>
+                <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24'>
+                 <g fill='none' stroke='#666666' stroke-linecap='round' stroke-linejoin='round' stroke-width='2'>
+                    <path d='M7 9.667A2.667 2.667 0 0 1 9.667 7h8.666A2.667 2.667 0 0 1 21 9.667v8.666A2.667 2.667 0 0 1 18.333 21H9.667A2.667 2.667 0 0 1 7 18.333z' />
+                    <path d='M4.012 16.737A2 2 0 0 1 3 15V5c0-1.1.9-2 2-2h10c.75 0 1.158.385 1.5 1' />
+                  </g>
+                </svg>
+              </button>
             </div>
           </div>
 
