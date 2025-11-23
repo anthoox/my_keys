cb587fa2bf6cc4b26b80db0ccd818e9323bf5eaf
Anthoox <kray.a4@gmail.com>
2025-11-23 13:52:36 +0100

    docs(views/services): documentar vista services.php y modularización de modales

--- app/views/services/services.php
+++ app/views/services/services.php
@@ -1,8 +1,11 @@
 <!-- view: services/services.php -->
+
+<!-- Cierre del contenedor abierto en layout/main.php -->
 </div>
+
 <!-- Botón flotante para añadir servicio -->
 <div class="position-fixed" style="bottom: 40px; right: 40px;">
-  <button type="button" class="btn btn-primary rounded-circle shadow" data-bs-toggle="modal" data-bs-target="#addServiceModal">
+  <button type="button" class="btn btn-primary rounded-circle shadow" data-bs-toggle="modal" data-bs-target="#addServiceModal" aria-label="Añadir servicio">
     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" viewBox="0 0 16 16">
       <path fill="#fbfbfb" d="M8 15c-3.86 0-7-3.14-7-7s3.14-7 7-7s7 3.14 7 7s-3.14 7-7 7M8 2C4.69 2 2 4.69 2 8s2.69 6 6 6s6-2.69 6-6s-2.69-6-6-6" />
       <path fill="#fbfbfb" d="M8 11.5c-.28 0-.5-.22-.5-.5V5c0-.28.22-.5.5-.5s.5.22.5.5v6c0 .28-.22.5-.5.5" />
@@ -13,6 +16,7 @@
 
 
 <?php
+// Incluir los modales relacionados con servicios (añadir, editar, eliminar)
 require_once __DIR__ . '/../../../app/views/components/modals/modalsServices.php';
 
 ?>
